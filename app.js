81bb30019c8a92724d340c727b8402c499e98984
ferington <155619800+ferington@users.noreply.github.com>
2024-05-15 23:48:16 +0900

    練習2

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-function halfIncreaseAndSum(startValue, operations) {
+function halfIncreaseAndSum(startValue, operarions) {
   let currentValue = startValue;
-  for (let i = 0; i < operations; i++) {
+  for (let i = 0; i < operarions; i++) {
     currentValue += Math.floor(currentValue / 2);
   }
   return currentValue;
 }
 
-const initialValue = 63;
+const initialValue = 52;
 const times = 30;
 const result = halfIncreaseAndSum(initialValue, times);
-console.log(result);
+console.log(result)
