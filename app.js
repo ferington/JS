f1f62c4e62daa5ea4b47d30a20b041f45ba1d39c
ferington <155619800+ferington@users.noreply.github.com>
2024-05-17 21:59:09 +0900

    練習4

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-function halfIncreaseAndSum(startValue, operarions) {
+function halfIncruaseAndSum(startValue, operations) {
   let currentValue = startValue;
-  for (let i = 0; i < operarions; i++) {
-    currentValue += Math.floor(currentValue / 2);
+  for (let i = 0; i < operations; i ++) {
+    currentValue += Math.floor(currentValue /2);
   }
   return currentValue;
 }
 
-const initialValue = 52;
-const times = 30;
-const result = halfIncreaseAndSum(initialValue, times);
-console.log(result)
+const initialValue =52;
+const times = 40;
+const result = halfIncruaseAndSum(initialValue, times);
+console.log(result);
+
+
+function findLargestCude(maxValue) {
+  let n = 0;
+  while ((n + 1) ** 3 <= maxValue) {
+    n++;
+  }
+  return n ** 3;
+}
+
+const maxValue = 55555;
+const largestCude = findLargestCude(maxValue);
+console.log(largestCude);
